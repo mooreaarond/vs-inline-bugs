73822ad0c359448b031006e4dc12ad75d929766a
Aaron Moore <mooreaarond@gmail.com>
2016-07-28 16:03:20 -0700

    initial commit, package creation no longer working for some reason

---
+++ VsInlineBugs/AddBugLinkSuggestedAction.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.Imaging.Interop;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace MooreAaronD.InlineBugs
+{
+    internal sealed class AddBugLinkSuggestedAction : ISuggestedAction
+    {
+        private readonly ITrackingSpan _span;
+        private readonly ITextSnapshot _snapshot;
+
+        
+        private string ReplacementValue { get; }
+
+        public AddBugLinkSuggestedAction(ITrackingSpan span)
+        {
+            _span = span;
+            _snapshot = span.TextBuffer.CurrentSnapshot;
+            string text = span.GetText(_snapshot);
+            ReplacementValue = Regex.Replace(text, "TODO", "TODO [BUG:<ID>]", RegexOptions.IgnoreCase);
+               
+
+            DisplayText = string.Format("Link this todo to a work item //TODO [bug:id]");
+        }
+
+        public string DisplayText { get; }
+
+        public string IconAutomationText => null;
+
+        ImageMoniker ISuggestedAction.IconMoniker => default(ImageMoniker);
+
+        public string InputGestureText => null;
+
+        public bool HasActionSets => false;
+
+        public Task<IEnumerable<SuggestedActionSet>> GetActionSetsAsync(CancellationToken cancellationToken) => null;
+
+        public bool HasPreview => true;
+
+        public Task<object> GetPreviewAsync(CancellationToken cancellationToken)
+        {
+            var textBlock = new TextBlock();
+            textBlock.Padding = new Thickness(5);
+            textBlock.Inlines.Add(new Run() { Text = ReplacementValue });
+            return Task.FromResult<object>(textBlock);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Invoke(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                return;
+
+            _span.TextBuffer.Replace(_span.GetSpan(_snapshot), ReplacementValue);
+        }
+
+        public bool TryGetTelemetryId(out Guid telemetryId)
+        {
+            telemetryId = Guid.Empty;
+            return false;
+        }
+    }
+}
---
+++ VsInlineBugs/TestSuggestedActionsSource.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.Text.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MooreAaronD.InlineBugs
+{
+    class TestSuggestedActionsSource : ISuggestedActionsSource
+    {
+        private readonly TestSuggestedActionsSourceProvider _factory;
+        private readonly ITextBuffer _textBuffer;
+        private readonly ITextView _textView;
+
+        internal static readonly Regex TodoNoBugRegex = new Regex(@"//\s*(?<todo>todo)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        internal static readonly Regex TodoWithBugRegex = new Regex(@"//\s*TODO\s*\[*BUG(\s*:\s*|\s*)\d+\]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public TestSuggestedActionsSource(TestSuggestedActionsSourceProvider testSuggestedActionsSourceProvider, ITextView textView, ITextBuffer textBuffer)
+        {
+            _factory = testSuggestedActionsSourceProvider;
+            _textBuffer = textBuffer;
+            _textView = textView;
+        }
+
+        #pragma warning disable 0067
+        public event EventHandler<EventArgs> SuggestedActionsChanged;
+        #pragma warning restore 0067
+
+        public void Dispose()
+        {
+        }
+
+        public IEnumerable<SuggestedActionSet> GetSuggestedActions(ISuggestedActionCategorySet requestedActionCategories, SnapshotSpan range, CancellationToken cancellationToken)
+        {
+            SnapshotSpan lineSnapshot;
+            if (!TryGetTodoLineSnapshotSpan(out lineSnapshot))
+                return Enumerable.Empty<SuggestedActionSet>();
+
+            ITrackingSpan todoTrackingSpan = range.Snapshot.CreateTrackingSpan(lineSnapshot, SpanTrackingMode.EdgeInclusive);
+            var addBugLinkAction = new AddBugLinkSuggestedAction(todoTrackingSpan);
+            return new SuggestedActionSet[] { new SuggestedActionSet(new ISuggestedAction[] { addBugLinkAction }) };
+        }
+
+        public Task<bool> HasSuggestedActionsAsync(ISuggestedActionCategorySet requestedActionCategories, SnapshotSpan range, CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                SnapshotSpan lineSnapshot;
+                return TryGetTodoLineSnapshotSpan(out lineSnapshot);
+            });
+        }
+
+        public bool TryGetTelemetryId(out Guid telemetryId)
+        {
+            telemetryId = Guid.Empty;
+            return false;
+        }
+
+        private bool TryGetTodoLineSnapshotSpan(out SnapshotSpan line)
+        {
+            line = default(SnapshotSpan);
+
+            ITextViewLine textViewLine = _textView.Caret.ContainingTextViewLine;
+            if (textViewLine == null)
+                return false;
+
+            string text = textViewLine.Extent.GetText() ?? string.Empty;
+            Match matchNoBug = TestSuggestedActionsSource.TodoNoBugRegex.Match(text);
+            Match matchWithBug = TestSuggestedActionsSource.TodoWithBugRegex.Match(text);
+
+            if (!matchNoBug.Success || matchWithBug.Success)
+                return false;
+
+            line = textViewLine.Extent;
+            return true;
+        }
+
+        private bool TryGetWordUnderCaret(out TextExtent wordExtent)
+        {
+            ITextCaret caret = _textView.Caret;
+            SnapshotPoint point;
+
+            if (caret.Position.BufferPosition > 0)
+            {
+                point = caret.Position.BufferPosition - 1;
+            }
+            else
+            {
+                wordExtent = default(TextExtent);
+                return false;
+            }
+
+            ITextStructureNavigator navigator = _factory.NavigatorService.GetTextStructureNavigator(_textBuffer);
+
+            wordExtent = navigator.GetExtentOfWord(point);
+            return true;
+        }
+    }
+}
